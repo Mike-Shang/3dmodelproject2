6b66761b976eb3213edadadcec35d4c4b2c32c5e
Mike-Shang <121717662+Mike-Shang@users.noreply.github.com>
2024-05-22 20:12:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/ikosgHd1eqyS9nRc/scene.splinecode');
